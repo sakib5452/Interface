eee509b033e38b6b55b5e966884cce87994300bc
najmul sakib <najmus191@gmail.com>
2023-12-05 12:28:51 +0600

    Inner

--- src/main/kotlin/NestedClass/Outer.kt
+++ src/main/kotlin/NestedClass/Outer.kt
@@ -2,8 +2,16 @@ package NestedClass
 
 class Outer {
     val a = "Outer Nested Class"
-    class Nested{
-        val b = "Inside Nested Class"
+//    class Nested{
+//        val b = "Inside Nested Class"
+//
+//        fun callMe() = "Function call from inside nested"
+//    }
+
+
+    inner class Nested{
+        val b = a
+
         fun callMe() = "Function call from inside nested"
     }
 
--- src/main/kotlin/NestedClass/main.kt
+++ src/main/kotlin/NestedClass/main.kt
@@ -5,7 +5,8 @@ fun main() {
     val outer = Outer()
     println(outer.a)
 
-    val nested = Outer.Nested()
+//    val nested = Outer.Nested()
+    val nested = outer.Nested() //inner ar jonno
     println(nested.b)
     println(nested.callMe())
 }
